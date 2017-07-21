86ef35cab82f271010e01cf4aa7429164e29b8ba
Kevin Mason <kevin.mason@outlook.com>
2017-07-21 12:36:34 -0500

    EssentialTools Listing 6-19 not working

--- EssentialTools/Models/LinqValueCalculator.cs
+++ EssentialTools/Models/LinqValueCalculator.cs
@@ -3,10 +3,18 @@ using System.Linq;
 
 namespace EssentialTools.Models
 {
-    public class LinqValueCalculator : IValueCalculator    {
+    public class LinqValueCalculator : IValueCalculator
+    {
+        private IDiscountHelper discounter;
+
+        public LinqValueCalculator (IDiscountHelper discountParam)
+        {
+           discounter = discountParam;
+        }
+
         public decimal ValueProducts (IEnumerable<Product> products)
         {
-            return products.Sum(p => p.Price);
+            return discounter.ApplyDiscount(products.Sum(p => p.Price));
         }
     }
 }
